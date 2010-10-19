750c9e17e5f8061110ca97123cb314716be8976e
Lucas Bruand <l.bruand.pro@gmail.com>
2010-10-19 23:38:31 +0200

    Added a new todo item.

--- src/main/scala/org/freetle/Transform.scala
+++ src/main/scala/org/freetle/Transform.scala
@@ -25,6 +25,7 @@ import javax.xml.namespace.QName
 //    and or wildcard cases and or a visitor pattern.
 //  * EvPositiveElement trashing is not satisfactory : We need to introduce a pattern for Result checking that
 //    encapsulate the EvPositiveElement trashing rather than have this code so widely occuring in the code.
+//  * With the namespaces, there is a problem. When you push tokens, how do u know which prefix to use ?
 //
 //	Optimizer
 //  =========
