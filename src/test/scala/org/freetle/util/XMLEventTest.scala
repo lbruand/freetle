3eb6771cc920ea1bf06ea68cbe817ddf279656b6
Lucas Bruand <l.bruand.pro@gmail.com>
2010-12-13 22:44:15 +0200

    Javadoc.

--- src/test/scala/org/freetle/util/XMLEventTest.scala
+++ src/test/scala/org/freetle/util/XMLEventTest.scala
@@ -1,10 +1,32 @@
+ /*
+  * Copyright 2010 Lucas Bruand
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package org.freetle.util
 
 import org.junit._
 import Assert._
 import org.freetle.{TestXMLHelperMethods, CPSXMLModel}
 
+/**
+ * Context specific to this test.
+ */
 case class XMLEventTestContext
+
+/**
+ * Testing what is specific to XMLEvents and related functions.
+ */
 @Test
 class XMLEventTest extends CPSXMLModel[XMLEventTestContext] with TestXMLHelperMethods[XMLEventTestContext] {
   @Test
