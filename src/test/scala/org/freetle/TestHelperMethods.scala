176592baddf1a564c2175d3ece07ea6f5326b26d
Lucas Bruand <l.bruand.pro@gmail.com>
2010-12-13 22:40:18 +0200

    Added Javadoc.

--- src/test/scala/org/freetle/TestHelperMethods.scala
+++ src/test/scala/org/freetle/TestHelperMethods.scala
@@ -1,9 +1,23 @@
+ /*
+  * Copyright 2010 Lucas Bruand
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package org.freetle
 import org.junit._
 import Assert._
 /**
- * Created by IntelliJ IDEA.
- * User: caroline
+ * Test helper class.
  */
 
 trait TestHelperMethods[Element, Context] extends CPSModel[Element, Context] {
