4d91fa015a98376b4bd8b6b72def558690851bb5
Lucas Bruand <l.bruand.pro@gmail.com>
2010-12-25 13:59:11 +0200

    Added STX Joost.

--- src/test/scala/org/freetle/BenchmarkTest.scala
+++ src/test/scala/org/freetle/BenchmarkTest.scala
@@ -24,6 +24,7 @@ import javax.xml.transform.stream.{StreamResult, StreamSource}
 import java.io.{StringWriter, StringReader}
 import testing.Benchmark
 import compat.Platform
+import net.sf.joost.trax.TransformerFactoryImpl
 
 
 @serializable @SerialVersionUID(599494944949L + 30000L)
@@ -169,6 +170,67 @@ class BenchmarkTest {
     var catalogSource : String = null
   }
 
+  class STXCaseBenchmark(override val nCatalog : Int = 3) extends FreetleBenchmark(nCatalog : Int) {
+    override val name = "stx"
+    override def prefix = "STX = "
+    val stxSource = """<?xml version="1.0" encoding="UTF-8"?>
+    <stx:transform xmlns:stx="http://stx.sourceforge.net/2002/ns"
+               xmlns:joost="http://joost.sf.net/extension"
+               xmlns:js="urn:javascript"
+               version="1.0">
+
+  <stx:template match="/">
+    <stx:element name="html">
+      <stx:element name="body">
+        <stx:process-children />        
+      </stx:element>
+    </stx:element>
+  </stx:template>
+
+  <stx:variable name="title" />
+  <stx:variable name="artist"/>
+
+  <stx:template match="title">
+    <stx:assign name="title" select="." />
+  </stx:template>
+  
+  <stx:template match="artist">
+    <stx:assign name="artist" select="." />
+  </stx:template>
+
+  <stx:template match="cd">
+          <stx:process-children />
+          <stx:element name="tr"><stx:value-of select="$title"/></stx:element><stx:text>
+</stx:text>
+          <stx:element name="tr"><stx:value-of select="$artist"/></stx:element><stx:text>
+</stx:text>
+  </stx:template>
+
+</stx:transform>
+    """
+    val transFactory = new TransformerFactoryImpl()
+    val transformerSource = new StreamSource(new StringReader(stxSource))
+
+    val transformer = transFactory.newTransformer(transformerSource)
+
+
+
+    var source : StreamSource = null
+
+
+    var resultStringWriter: StringWriter = null
+
+
+    def run() = {
+      source = new StreamSource(new StringReader(new String(catalogSource.getBytes)))
+      resultStringWriter = new StringWriter()
+      val result = new StreamResult(resultStringWriter)
+      transformer.transform(source, result)
+      val resultString: String = resultStringWriter.toString
+      //assertEquals("", resultString)
+    }
+    
+  }
   
   class XSLTCaseBenchmark(override val nCatalog : Int = 3) extends FreetleBenchmark(nCatalog : Int) {
     override val name = "xslt"
@@ -248,23 +310,28 @@ class BenchmarkTest {
   def testXSLT() = {
     val warmup = 4
     val testRetries = 5
-    //val sizes = (2 to 4).map(_ * 2500)
-    val sizes = Stream(10000)
-    val benchmarks = Stream.concat(
-                    (sizes).map(x => new XSLTCaseBenchmark(nCatalog = x)),
-                    (sizes).map(x => new IdentityCaseBenchmark(nCatalog = x)),
-                    (sizes).map(x => new FreetleCaseBenchmark(nCatalog = x))
-                    
-                  )
-
-    val timings = benchmarks.map(x => {
+    val sizes = (3 to 5).map(_ * 2500)
+    //val sizes = Stream(1000)
+    val benchmarks = List(
+         (x :Int) => new XSLTCaseBenchmark(nCatalog = x),
+         (x :Int) => new IdentityCaseBenchmark(nCatalog = x),
+         (x :Int) => new STXCaseBenchmark(nCatalog = x),
+         (x :Int) => new FreetleCaseBenchmark(nCatalog = x)
+      )
+    val benchmarksRun = benchmarks.flatMap(f => sizes.map(f))
+    val timings = benchmarksRun.map(x => {
         x.catalogSource = buildRandomCatalog(x.nCatalog)
         Platform.collectGarbage
         val r = (x.name, x.nCatalog, x.catalogSource.length, (x.runBenchmark(warmup + testRetries).drop(warmup).sum/testRetries))
         x.catalogSource = null
         r
       }).toList
-    println(timings)
+    //println(timings)
+    timings.foreach(
+        x => {
+          println(x._1 + ";" + x._2 + ";" + x._3 + ";" + x._4)
+        }
+      )
   }
 
 
