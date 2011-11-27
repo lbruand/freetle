460f7b78effee6334155d3de8f4580c8b4e957dd
Lucas Bruand <l.bruand.pro@gmail.com>
2011-11-27 13:34:36 +0100

    started multifile

---
+++ freetlelib/src/test/scala/org/freetle/util/FileSplitterTest.scala
@@ -0,0 +1,65 @@
+ /*
+  * Copyright 2010-2011 Lucas Bruand
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package org.freetle.util
+import org.junit._
+import Assert._
+import org.freetle.{TestXMLHelperMethods, CPSXMLModel}
+import java.io._
+import org.freetle.CPSModelTypeDefinition._
+
+case class FileSplitterContext()
+@Test
+class FileSplitterTest extends CPSXMLModel[FileSplitterContext]{
+  val fileMatcher : ChainedTransformRoot = (takeSpace*) ~ <("File") ~ new DeepFilter()
+  /**
+   * Serialise a XMLResultStream into a XML form.
+   */
+  def serializeXMLResultStream(evStream : =>CPSStream,
+                               writerConstructor : (Int, Writer) => Writer,
+                               occurrence : Int = 0,
+                               writerInput : Writer = null) : Unit = {
+    val trans = fileMatcher(new CFilterIdentity(), new CFilterIdentity())
+    var that = trans(CPSStreamHelperMethods.turnToTail(evStream), null)
+    val writer = writerConstructor(occurrence, writerInput)
+    while (!that.isEmpty && that.head._2) {
+      _._1 match {
+              case Some(x : XMLEvent) => x.appendWriter(writer)
+              case _ => (new EvComment("EmptyPositive")).appendWriter(writer)
+              }
+      that = that.tail
+    }
+
+    writer.flush()
+    if (!that.isEmpty) {
+      serializeXMLResultStream(that,
+                               writerConstructor = writerConstructor,
+                               occurrence = occurrence + 1,
+                               writer)
+    }
+  }
+
+
+  @Test
+  def test() = {
+    """
+    <File>euhgzuie zeufhenuzehf fzehfeuiezh </File>
+    <File>euhgzuie zeufhenuzehf fzehfeuiezh </File>
+    <File>euhgzuie zeufhenuzehf fzehfeuiezh </File>
+    """
+
+  }
+
+}
