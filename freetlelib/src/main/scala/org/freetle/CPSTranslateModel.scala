4b5da8a0a317ebb0dbdaf3d53372246aeebb118e
Lucas Bruand <l.bruand.pro@gmail.com>
2011-12-13 21:42:28 +0100

    scaladoc

--- freetlelib/src/main/scala/org/freetle/CPSTranslateModel.scala
+++ freetlelib/src/main/scala/org/freetle/CPSTranslateModel.scala
@@ -22,7 +22,7 @@ import io.Source
 
 
 /**
- * This model is used to transform a Stream[Char] towards a Stream[XMLEvent]
+ * This model is extended to transform a Stream[Char] towards a Stream[XMLEvent]
  */
 class CPSTranslateModel[Context] extends CPSModel[Either[Char, XMLEvent], Context] {
 
